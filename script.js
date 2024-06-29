8be02951dd1435ebb20752c0ccc3e6bc5911d435
AlexisCandia500 <alexisfigueredo0@gmail.com>
2024-06-28 23:06:15 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -48,20 +48,20 @@ function init() {
     }
 
     function validarInput() {
-    const intento = leerIntento().trim();
-    const LETRAS = /^[a-zA-Z]+$/;
-    if (intento.length !== 5) {
-        ERROR.innerHTML = "*Ingrese exactamente 5 caracteres";
-        input.style.borderColor = 'red';
-    } else if (!LETRAS.test(intento)) {
-        ERROR.innerHTML = "*Solo se admite letras";
-        input.style.borderColor = 'red';
-    } else {
-        ERROR.innerHTML = "";
-        input.style.borderColor = '#ccc';
-        intentar();
+        const intento = leerIntento().trim();
+        const LETRAS = /^[a-zA-Z]+$/;
+        if (intento.length !== 5) {
+            ERROR.innerHTML = "*Ingrese exactamente 5 caracteres";
+            input.style.borderColor = 'red';
+        } else if (!LETRAS.test(intento)) {
+            ERROR.innerHTML = "*Solo se admite letras";
+            input.style.borderColor = 'red';
+        } else {
+            ERROR.innerHTML = "";
+            input.style.borderColor = '#ccc';
+            intentar();
+        }
     }
-}
 
     function intentar() {
         const intento = leerIntento();
