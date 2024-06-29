3c2e6e5f458aead219a1f43be9ed92d4adf4b0cb
AlexisCandia500 <alexisfigueredo0@gmail.com>
2024-06-29 14:16:54 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ window.addEventListener('load', init);
 function init() {
     let intentos;
     let palabra = '';
-    const diccionario = ['MADRE','ADIOS'];
+    const diccionario = ['MADRE', 'HIJOS', 'PADRE', 'ADIOS'];
     const VIDA = document.getElementById("vida");
     const GRID = document.getElementById("grid");
     const ERROR = document.getElementById("error");
     const button = document.getElementById("guess-button");
     const input = document.getElementById("guess-input");
-    const contenedorMensajes = document.getElementById('guesses'); // Contenedor de mensajes de fin del juego
+    const contenedorMensajes = document.getElementById('guesses');
 
     iniciarJuego();
 
     function iniciarJuego() {
         intentos = 6;
-        palabra = '';
         VIDA.innerHTML = intentos;
         input.disabled = false;
         input.value = '';
         ERROR.innerHTML = '';
-        contenedorMensajes.innerHTML = ''; // Limpiar el mensaje de fin del juego
+        contenedorMensajes.innerHTML = '';
         GRID.innerHTML = '';
         button.innerText = "Adivinar";
         button.removeEventListener('click', reiniciarJuego);
@@ -77,7 +76,7 @@ function init() {
         ROW.className = 'row';
 
         if (intento === palabra || diccionario.includes(intento)) {
-            for (let i = 0; i < palabra.length; i++) {
+            for (let i = 0; i < 5; i++) {
                 const SPAN = document.createElement('div');
                 SPAN.className = 'row-letter';
                 SPAN.innerHTML = intento[i];
@@ -85,11 +84,10 @@ function init() {
                 SPAN.style.border = '1px solid #79b851';
                 ROW.appendChild(SPAN);
             }
-
             GRID.appendChild(ROW);
             terminar("<h1>¡GANASTE!😀</h1>");
         } else {
-            for (let i = 0; i < palabra.length; i++) {
+            for (let i = 0; i < 5; i++) {
                 const SPAN = document.createElement('div');
                 SPAN.className = 'row-letter';
 
